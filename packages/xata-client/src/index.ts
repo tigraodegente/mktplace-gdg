d75145763886e62ea98ad0796c7ed5d5519107c6
Gustavo <ti@graodegente@gmail.com>
2025-05-26 16:26:44 -0300

    feat: integra Xata.io com tabelas e dados de exemplo

---
+++ packages/xata-client/src/index.ts
@@ -0,0 +1,12 @@
+export * from './xata';
+
+// Re-export types for convenience
+export type {
+  CategoriesRecord,
+  OrderItemsRecord,
+  OrdersRecord,
+  ProductsRecord,
+  SellersRecord,
+  UsersRecord,
+  DatabaseSchema
+} from './xata'; 
