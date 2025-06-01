607f3f2c8a0983cdf3bde9bd2283a6c65de97961
Gustavo <ti@graodegente@gmail.com>
2025-06-01 18:13:46 -0300

    chore: grande limpeza - remove Hyperdrive, Railway, Fly e outros serviços desnecessários. Remove pacote db-hyperdrive, scripts Railway, documentação Hyperdrive, rotas debug. Simplifica database.ts para apenas Neon + Local. Mantém Local PostgreSQL, Neon produção, Cloudflare hospedagem

--- scripts/04-view-data-summary.mjs
+++ scripts/04-view-data-summary.mjs
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import 'dotenv/config'
-import { Database } from '../packages/db-hyperdrive/dist/index.js'
+import { Database } from '../apps/store/src/lib/db/database.js'
 
 console.log('📊 Resumo dos dados do marketplace\n')
 
